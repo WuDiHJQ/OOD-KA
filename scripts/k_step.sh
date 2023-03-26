599b672938bd708a900ad96021c444aad6ee4b71
“WuDiHJQ” <jiaqihan@zju.edu.cn>
2023-03-26 12:48:03 +0800

    update

---
+++ scripts/k_step.sh
@@ -0,0 +1,6 @@
+# k step select
+python PFA.py --unlabeled svhn -k 1
+python PFA.py --unlabeled svhn -k 2
+python PFA.py --unlabeled svhn -k 3
+python PFA.py --unlabeled svhn -k 4
+python PFA.py --unlabeled svhn -k 5
