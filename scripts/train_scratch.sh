c99349359fca2510ac13f34244362e93a887a065
“WuDiHJQ” <jiaqihan@zju.edu.cn>
2023-03-31 00:50:20 +0800

    update

--- scripts/train_scratch.sh
+++ scripts/train_scratch.sh
@@ -1,6 +1,15 @@
 # train scratch
-python train_scratch.py --lr 0.1 --batch-size 128 --model resnet34 --dataset cifar100_part0 --gpu 0
-python train_scratch.py --lr 0.1 --batch-size 128 --model resnet34 --dataset cifar100_part1 --gpu 0
+python train_scratch.py --lr 0.1 --batch-size 128 --model vgg16 --dataset cifar100_part0 --gpu 0
+python train_scratch.py --lr 0.1 --batch-size 128 --model vgg16 --dataset cifar100_part1 --gpu 0
+
+
+python train_scratch.py --lr 0.1 --batch-size 128 --model resnet18 --dataset cifar100_part0 --gpu 0
+python train_scratch.py --lr 0.1 --batch-size 128 --model resnet18 --dataset cifar100_part1 --gpu 0
+
+
+python train_scratch.py --lr 0.1 --batch-size 128 --model wrn40_2 --dataset cifar100_part0 --gpu 0
+python train_scratch.py --lr 0.1 --batch-size 128 --model wrn40_2 --dataset cifar100_part1 --gpu 0
+
 
 python train_scratch.py --lr 0.1 --batch-size 128 --model wrn16_2 --dataset cifar100_part0 --gpu 0
 python train_scratch.py --lr 0.1 --batch-size 128 --model wrn16_2 --dataset cifar100_part1 --gpu 0
