c9f4a5ea7aaf1f42f8fae80e49301fcb96b88d07
“WuDiHJQ” <jiaqihan@zju.edu.cn>
2023-03-31 14:54:01 +0800

    update

---
+++ scripts/PFA.sh
@@ -0,0 +1,7 @@
+# PFA reproduce
+python PFA.py --model wrn16_2 --unlabeled cifar10
+python PFA.py --model wrn16_2 --unlabeled svhn
+
+
+python PFA.py --model vgg16 --unlabeled cifar10
+python PFA.py --model vgg16 --unlabeled svhn
--- scripts/k_step.sh
+++ scripts/k_step.sh
@@ -1,6 +1,8 @@
 # k step select
-python PFA.py --unlabeled svhn -k 1
-python PFA.py --unlabeled svhn -k 2
-python PFA.py --unlabeled svhn -k 3
-python PFA.py --unlabeled svhn -k 4
-python PFA.py --unlabeled svhn -k 5
+python PFA.py --model wrn16_2 --unlabeled svhn -k 1
+python PFA.py --model wrn16_2 --unlabeled svhn -k 2
+python PFA.py --model wrn16_2 --unlabeled svhn -k 3
+python PFA.py --model wrn16_2 --unlabeled svhn -k 4
+python PFA.py --model wrn16_2 --unlabeled svhn -k 5
+python PFA.py --model wrn16_2 --unlabeled svhn -k 6
+python PFA.py --model wrn16_2 --unlabeled svhn -k 7
