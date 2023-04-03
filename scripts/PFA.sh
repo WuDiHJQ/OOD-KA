3f69bf69f62c274dfd9486b2abe4b75463209b80
“WuDiHJQ” <jiaqihan@zju.edu.cn>
2023-04-03 13:21:17 +0800

    update

--- scripts/PFA.sh
+++ scripts/PFA.sh
@@ -1,7 +1,15 @@
 # PFA reproduce
-python PFA.py --model wrn16_2 --unlabeled cifar10
-python PFA.py --model wrn16_2 --unlabeled svhn
-
-
 python PFA.py --model vgg16 --unlabeled cifar10
 python PFA.py --model vgg16 --unlabeled svhn
+
+
+python PFA.py --model resnet18 --unlabeled cifar10
+python PFA.py --model resnet18 --unlabeled svhn
+
+
+python PFA.py --model wrn40_2 --unlabeled cifar10
+python PFA.py --model wrn40_2 --unlabeled svhn
+
+
+python PFA.py --model wrn16_2 --unlabeled cifar10
+python PFA.py --model wrn16_2 --unlabeled svhn
---
+++ scripts/kd_vanilla.sh
@@ -0,0 +1,15 @@
+# KD vanilla
+python kd_vanilla.py --model vgg16 --unlabeled cifar10
+python kd_vanilla.py --model vgg16 --unlabeled svhn
+
+
+python kd_vanilla.py --model resnet18 --unlabeled cifar10
+python kd_vanilla.py --model resnet18 --unlabeled svhn
+
+
+python kd_vanilla.py --model wrn40_2 --unlabeled cifar10
+python kd_vanilla.py --model wrn40_2 --unlabeled svhn
+
+
+python kd_vanilla.py --model wrn16_2 --unlabeled cifar10
+python kd_vanilla.py --model wrn16_2 --unlabeled svhn
