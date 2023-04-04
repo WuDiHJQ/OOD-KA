3404b34fd1a2ee7abee2c90e3a797efe824908db
“WuDiHJQ” <jiaqihan@zju.edu.cn>
2023-04-04 19:12:38 +0800

    update

--- scripts/k_step.sh
+++ scripts/k_step.sh
@@ -5,4 +5,11 @@ python PFA.py --model wrn16_2 --unlabeled svhn -k 3
 python PFA.py --model wrn16_2 --unlabeled svhn -k 4
 python PFA.py --model wrn16_2 --unlabeled svhn -k 5
 python PFA.py --model wrn16_2 --unlabeled svhn -k 6
-python PFA.py --model wrn16_2 --unlabeled svhn -k 7
+
+
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 1
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 2
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 3
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 4
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 5
+python PFA.py --model wrn16_2 --unlabeled cifar10 -k 6
---
+++ scripts/kd_ood.sh
@@ -0,0 +1,23 @@
+# KD OOD
+python kd_ood.py --model vgg16 --unlabeled cifar10
+python kd_ood.py --model vgg16 --unlabeled svhn
+python kd_ood.py --model vgg16 --unlabeled imagenet_32x32
+python kd_ood.py --model vgg16 --unlabeled places365_32x32
+
+
+python kd_ood.py --model resnet18 --unlabeled cifar10
+python kd_ood.py --model resnet18 --unlabeled svhn
+python kd_ood.py --model resnet18 --unlabeled imagenet_32x32
+python kd_ood.py --model resnet18 --unlabeled places365_32x32
+
+
+python kd_ood.py --model wrn40_2 --unlabeled cifar10
+python kd_ood.py --model wrn40_2 --unlabeled svhn
+python kd_ood.py --model wrn40_2 --unlabeled imagenet_32x32
+python kd_ood.py --model wrn40_2 --unlabeled places365_32x32
+
+
+python kd_ood.py --model wrn16_2 --unlabeled cifar10
+python kd_ood.py --model wrn16_2 --unlabeled svhn
+python kd_ood.py --model wrn16_2 --unlabeled imagenet_32x32
+python kd_ood.py --model wrn16_2 --unlabeled places365_32x32
